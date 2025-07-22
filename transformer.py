ff6b0ebbc0310e3a0930601a550345a2451190e0
Billy Newport <billy@billynewport.com>
2025-07-22 15:50:51 -0400

    Fixing transformer

--- transformer.py
+++ transformer.py
@@ -1,5 +1,6 @@
-
+from sqlalchemy import Connection
+from typing import Dict
 
 
 def executeTransformer(conn: Connection, tableMapping: Dict[str, str]) -> None:
-  pass
+    print(f"Executing transformer with {tableMapping}")
